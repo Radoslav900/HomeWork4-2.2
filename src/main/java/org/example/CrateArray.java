ed328a30ddd6262421d31896fd3a272eec8ed5d2
DESKTOP-I65T3K3\lenovo <realsteelatomst@gmail.com>
2023-11-27 14:06:21 +0200

    Array

---
+++ src/main/java/org/example/CrateArray.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class CrateArray {
+    public static void main(String[] args){
+        int [] numbers = new int[20];
+
+        for (int i = 0; i < 20; i++){
+            int number = i * 5;
+            numbers[i] = number;
+        }
+
+        for (int k = 0; k < 20; k++){
+            int numberOfArray = numbers[k];
+            System.out.printf("Index: %d; Number: %d.\n", k, numberOfArray);
+        }
+    }
+
+}
